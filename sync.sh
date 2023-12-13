bc6daf2e30d0fd0f76088f31787c9f0f0b1213d7
Evan Heyborne <evansheyborne@gmail.com>
2023-12-12 17:39:49 -0700

    Module importer

---
+++ sync.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+count=0
+changed=()
+
+git submodule foreach '
+    echo "Updating submodule: $path"
+    bash -c "
+        git checkout main  # Switch to the main branch (replace with the appropriate branch name)
+        git pull origin main  # Update to the latest commit on the main branch
+        echo \"--------------\"
+        count=$((count + 1))
+        if [[ -n \$(git status --porcelain) ]]; then
+            changed+=(\"\$(basename \"\$path\")\")
+        fi
+    "
+'
+
+echo "$count submodules changed"
+
+for module in "${changed[@]}"; do
+    printf "\t%s\n" "$module"
+done
